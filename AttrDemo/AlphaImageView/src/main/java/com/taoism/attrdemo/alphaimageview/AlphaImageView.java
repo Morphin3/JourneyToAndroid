65088ee25331fde025ff04799c50303aca5f5e86
Morphin3 <fei.chen@123feng.com>
2014-09-18 15:13:10 +0800

    feat(Demo):增加属性(Attribue)资源Demo

---
+++ AttrDemo/AlphaImageView/src/main/java/com/taoism/attrdemo/alphaimageview/AlphaImageView.java
@@ -0,0 +1,15 @@
+package com.taoism.attrdemo.alphaimageview;
+
+import android.widget.ImageView;
+
+/**
+ * Created by chenfei on 14-9-18.
+ */
+public class AlphaImageView extends ImageView {
+    //    图像透明度每次改变的大小
+    private int alphaDelta = 0;
+    记录当前图片的透明度
+    private int curAlpha = 0;
+
+
+}
