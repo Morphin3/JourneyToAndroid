7fe11f49b3bc5935100c6efd436feff5096742d3
Morphin3 <morphin3@aliyun.com>
2014-10-22 17:27:43 +0800

    feat(Application):添加MyApplication类

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/MyApplication.java
@@ -0,0 +1,13 @@
+package com.taoism.journeytoandroid;
+
+import android.app.Application;
+
+/**
+ * Created by chenfei on 14-10-22.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/activeandroid/ActiveAndroidDemo.java
@@ -0,0 +1,18 @@
+package com.taoism.journeytoandroid.activeandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.taoism.journeytoandroid.R;
+
+/**
+ * Created by chenfei on 14-10-22.
+ */
+public class ActiveAndroidDemo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_activeandroid);
+    }
+
+
+}
