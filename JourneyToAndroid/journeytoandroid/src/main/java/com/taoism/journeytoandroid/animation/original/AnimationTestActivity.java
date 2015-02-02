789e16d52dbc33ee314c03499b5df54398ef3f6a
Morphin3 <morphin3@aliyun.com>
2015-02-02 10:44:13 +0800

    feat(AnimationTest):增加开始停止按钮

--- JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/animation/original/AnimationTestActivity.java
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/animation/original/AnimationTestActivity.java
@@ -6,21 +6,26 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.taoism.journeytoandroid.R;
+import com.taoism.journeytoandroid.interfaces.InitializeInterface;
 import com.taoism.journeytoandroid.utils.Constant;
 
 /**
  * Created by Morphin3 on 1/29/15.
  */
-public class AnimationTestActivity extends Activity {
+public class AnimationTestActivity extends Activity implements InitializeInterface,View.OnClickListener {
 
+    private Button btn_start;
+    private Button btn_stop;
     private FrameLayout fl_countdown;
     private ImageView iv_countdown_front;
     private TextView tv_countdown;
 
+    private CountDownTimer mCountDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +33,48 @@ public class AnimationTestActivity extends Activity {
         setContentView(R.layout.activity_animation_test);
 
         initCustom();
+        initListener();
     }
 
-    private void initCustom(){
+    @Override
+    public void initCustom(){
+        btn_start=(Button)findViewById(R.id.btn_start);
+        btn_stop=(Button)findViewById(R.id.btn_stop);
         fl_countdown=(FrameLayout)findViewById(R.id.fl_countdown);
         iv_countdown_front=(ImageView)findViewById(R.id.iv_countdown_front);
         tv_countdown=(TextView)findViewById(R.id.tv_countdown);
 
-        Animation countdown= AnimationUtils.loadAnimation(this,R.anim.countdown);
-        iv_countdown_front.startAnimation(countdown);
+    }
 
-        new CountDownTimer(Constant.Time.SECOND*60,Constant.Time.SECOND){
-            @Override
-            public void onTick(long millisUntilFinished) {
-                tv_countdown.setText(millisUntilFinished/1000+"");
-            }
+    @Override
+    public void initListener() {
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
 
-            @Override
-            public void onFinish() {
-                fl_countdown.setVisibility(View.GONE);
-            }
-        }.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int rid=v.getId();
+        if(rid==R.id.btn_start){
+            fl_countdown.setVisibility(View.VISIBLE);
+            Animation countdown= AnimationUtils.loadAnimation(this,R.anim.countdown);
+            iv_countdown_front.startAnimation(countdown);
+
+            mCountDownTimer=new CountDownTimer(Constant.Time.SECOND*60,Constant.Time.SECOND){
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    tv_countdown.setText(millisUntilFinished/1000+"");
+                }
+
+                @Override
+                public void onFinish() {
+                    fl_countdown.setVisibility(View.GONE);
+                }
+            }.start();
+        }else if(rid==R.id.btn_stop){
+            iv_countdown_front.clearAnimation();
+            mCountDownTimer.cancel();
+        }
     }
 }
