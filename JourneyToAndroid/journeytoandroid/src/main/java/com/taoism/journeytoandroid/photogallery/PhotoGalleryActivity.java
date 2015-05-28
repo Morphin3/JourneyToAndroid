3c0e51e3a07489aa850de91f32690c8343c28fc7
Morphin3 <morphin3@aliyun.com>
2015-05-28 15:24:49 +0800

    feat(baseclass)Add SingleFragmentActivity

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,28 @@
+package com.taoism.journeytoandroid.photogallery;
+
+import android.support.v4.app.Fragment;
+
+import com.taoism.journeytoandroid.baseclass.SingleFragmentActivity;
+
+/**
+ * Date: 2015-05-28
+ * Time: 15:04
+ * Author: Morphin3
+ * WeChat: 398788401
+ * E-mail: morphin333@gmail.com
+ * -----------------------------
+ * FIXME
+ */
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+
+
+
+
+
+    //返回fragment实例
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
