883d43d1d01db43d4188beef0dbea1975fc04968
Morphin3 <hzchenfei15@corp.netease.com>
2015-06-30 23:02:59 +0800

    feat(customview):Add ExpandableTextView and its demo.

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/view/recycleview/RecycleViewDemoActivity.java
@@ -0,0 +1,23 @@
+package com.taoism.journeytoandroid.view.recycleview;
+
+import android.os.Bundle;
+
+import com.taoism.journeytoandroid.BaseActivity;
+
+/**
+ * Date: 2015-06-21
+ * Time: 14:44
+ * Author: cf
+ * -----------------------------
+ * FIXME
+ */
+public class RecycleViewDemoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+    }
+
+
+}
