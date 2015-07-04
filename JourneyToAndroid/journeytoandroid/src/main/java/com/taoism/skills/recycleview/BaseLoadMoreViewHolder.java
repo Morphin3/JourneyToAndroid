758fa7cfaa88d675f9ae31d44fc1df1e2d8c215b
Morphin3 <hzchenfei15@corp.netease.com>
2015-07-04 22:10:34 +0800

    feat(customview):Add SortRecyclerViewDemo

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/skills/recycleview/BaseLoadMoreViewHolder.java
@@ -0,0 +1,19 @@
+package com.taoism.skills.recycleview;
+
+import android.view.View;
+
+/**
+ * Created by zhengwen on 15-6-19.
+ */
+public class BaseLoadMoreViewHolder extends TRecycleViewHolder {
+    public BaseLoadMoreViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void inflate() {
+    }
+
+    @Override
+    public void refresh(TAdapterItem item) {}
+}
