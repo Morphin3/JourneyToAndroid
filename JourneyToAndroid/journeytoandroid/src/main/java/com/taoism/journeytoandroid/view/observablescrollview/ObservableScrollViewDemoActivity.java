7b35cf177ad881f793b6bfd64eb260d7b6545847
Morphin3 <morphin3@aliyun.com>
2015-09-08 20:41:51 +0800

    Add FlexibleSpaceWithImageWithViewPagerTabActivity

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/view/observablescrollview/ObservableScrollViewDemoActivity.java
@@ -0,0 +1,25 @@
+package com.taoism.journeytoandroid.view.observablescrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.taoism.journeytoandroid.R;
+
+/**
+ * Date: 2015-09-08
+ * Time: 16:34
+ * Author: cf
+ * -----------------------------
+ */
+public class ObservableScrollViewDemoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observablescrollview_demo);
+    }
+
+
+
+}
