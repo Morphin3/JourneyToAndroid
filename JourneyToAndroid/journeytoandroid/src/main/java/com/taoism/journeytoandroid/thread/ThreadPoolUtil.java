bc9a3e89a371d924a1179f6ba8fe4dc722fc2687
Morphin3 <morphin333@gmail.com>
2017-03-12 18:16:21 +0800

    add ThreadPoolUtil

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/thread/ThreadPoolUtil.java
@@ -0,0 +1,45 @@
+package com.taoism.journeytoandroid.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Date: 2017-03-01
+ * Time: 15:34
+ * Author: cf
+ * -----------------------------
+ */
+
+public class ThreadPoolUtil {
+
+
+    private static volatile ExecutorService mExecutorService;
+
+    private ThreadPoolUtil() {
+
+    }
+
+    public static ExecutorService getInstance() {
+        if (mExecutorService == null) {
+            synchronized (ThreadPoolUtil.class) {
+                if (mExecutorService == null) {
+//                    Executors.newFixedThreadPool(5, new ThreadFactory() {
+//                        @Override
+//                        public Thread newThread(Runnable r) {
+//                            return null;
+//                        }
+//                    });
+                    mExecutorService = Executors.newCachedThreadPool();
+                }
+            }
+        }
+        return mExecutorService;
+    }
+
+
+    public static void execute(Runnable runnable) {
+        getInstance().execute(runnable);
+    }
+
+
+}
