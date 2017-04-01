9dce35eef6249b45746e358873b77ea079e94388
Morphin3 <morphin333@gmail.com>
2017-04-01 10:18:25 +0800

    Fresco Demo

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/canvas/FrescoDemo.java
@@ -0,0 +1,23 @@
+package com.taoism.journeytoandroid.canvas;
+
+import android.os.Bundle;
+
+import com.taoism.journeytoandroid.BaseActivity;
+
+/**
+ * Date: 2017-03-22
+ * Time: 16:19
+ * Author: cf
+ * -----------------------------
+ */
+
+public class FrescoDemo extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
