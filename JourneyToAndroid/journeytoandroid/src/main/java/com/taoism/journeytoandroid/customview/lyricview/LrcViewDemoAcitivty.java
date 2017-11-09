e3a710301c87475793a9e61445a7d361d9392a8f
Morphin3 <morphin333@gmail.com>
2017-11-09 18:05:30 +0800

    add SongTextView demo

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/customview/lyricview/LrcViewDemoAcitivty.java
@@ -0,0 +1,24 @@
+package com.taoism.journeytoandroid.customview.lyricview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.taoism.journeytoandroid.R;
+
+/**
+ * Date: 2017-11-09
+ * Time: 15:53
+ * Author: cf
+ * -----------------------------
+ */
+
+public class LrcViewDemoAcitivty extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lrc_view_demo);
+    }
+
+}
