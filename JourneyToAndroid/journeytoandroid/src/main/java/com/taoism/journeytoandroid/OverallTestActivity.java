483b708d04282d3f9968085417bcb0014b7feb2a
Morphin3 <morphin333@gmail.com>
2017-11-09 15:33:51 +0800

    "exclude module" test

---
+++ JourneyToAndroid/journeytoandroid/src/main/java/com/taoism/journeytoandroid/OverallTestActivity.java
@@ -0,0 +1,20 @@
+package com.taoism.journeytoandroid;
+
+import android.os.Bundle;
+
+/**
+ * Date: 2017-08-23
+ * Time: 13:42
+ * Author: cf
+ * -----------------------------
+ */
+
+public class OverallTestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overall_test);
+    }
+
+}
